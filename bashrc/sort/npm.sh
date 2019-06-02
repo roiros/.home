b2f66ff0ced18683c760b3ef58d3324936ddea00
Roi Rosenthal <roir@wix.com>
2019-06-02 17:48:34 +0300

    NOQA|add unlink functionality

--- bashrc/sort/npm.sh
+++ bashrc/sort/npm.sh
@@ -1,2 +1,3 @@
 alias linked="ls -l node_modules/|grep "^l"|sed -e 's/.* \([^ ][^ ]*\) ->/ \1 ->/'"
+alias npmunlink="linked |sed -e 's/ ->.*//'|xargs yarn unlink"
 alias lastVer='npm info $(jq -r .name package.json) --json versions|jq -r ".[]" |sort -V|tail -1'
